2d2b945ec81d5212d8d7afa51f044c9a680bb68c
4tendev <4tenprog@gmail.com>
2026-07-11 12:58:37 +0330

    Update 003_add_profile_image_to_users.sql

--- db/migrations/003_add_profile_image_to_users.sql
+++ db/migrations/003_add_profile_image_to_users.sql
@@ -6,16 +6,6 @@ SET profile_image = '/avatar.png'
 WHERE profile_image IS NULL
    OR length(trim(profile_image)) = 0;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_constraint
-    WHERE conname = 'users_profile_image_not_blank'
-  ) THEN
-    ALTER TABLE users
-      ADD CONSTRAINT users_profile_image_not_blank
-      CHECK (length(trim(profile_image)) > 0);
-  END IF;
-END;
-$$;
+ALTER TABLE users
+ADD CONSTRAINT users_profile_image_not_blank
+CHECK (length(trim(profile_image)) > 0);
